7d16d22c58f572617f051fbf196bd1fad601db18
SAJID LATIF <indus1530@gmail.com>
2021-09-30 05:01:35 +1400

    SectionW1Activity created

---
+++ app/src/main/java/edu/aku/hassannaqvi/lhwevaluation/ui/sections/SectionW1Activity.java
@@ -0,0 +1,86 @@
+package edu.aku.hassannaqvi.lhwevaluation.ui.sections;
+
+import static edu.aku.hassannaqvi.lhwevaluation.core.MainApp.form;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+
+import edu.aku.hassannaqvi.lhwevaluation.MainActivity;
+import edu.aku.hassannaqvi.lhwevaluation.R;
+import edu.aku.hassannaqvi.lhwevaluation.contracts.TableContracts;
+import edu.aku.hassannaqvi.lhwevaluation.core.MainApp;
+import edu.aku.hassannaqvi.lhwevaluation.database.DatabaseHelper;
+import edu.aku.hassannaqvi.lhwevaluation.databinding.ActivitySectionW1Binding;
+
+
+public class SectionW1Activity extends AppCompatActivity {
+    private static final String TAG = "SectionW1Activity";
+    ActivitySectionW1Binding bi;
+    private DatabaseHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_w1);
+        bi.setCallback(this);
+        bi.setForm(form);
+
+    }
+
+
+    private boolean updateDB() {
+        db = MainApp.appInfo.getDbHelper();
+        long updcount = 0;
+        try {
+            updcount = db.updatesFormColumn(TableContracts.FormsTable.COLUMN_SW1, form.sW1toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d(TAG, R.string.upd_db_form + e.getMessage());
+            Toast.makeText(this, R.string.upd_db_form + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (updcount > 0) return true;
+        else {
+            Toast.makeText(this, R.string.upd_db_error, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    public void btnContinue(View view) {
+        if (!formValidation()) return;
+        if (updateDB()) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        } else Toast.makeText(this, R.string.fail_db_upd, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void btnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
+        setResult(RESULT_CANCELED);
+    }
+
+
+}
