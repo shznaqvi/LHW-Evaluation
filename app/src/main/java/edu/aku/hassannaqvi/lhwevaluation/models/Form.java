ad067e09e4f13eec476aae5dda0f3ac577288a40
SAJID LATIF <indus1530@gmail.com>
2021-10-06 15:58:27 +0500

    Form & activity_section_b1.xml b103Skip modified

--- app/src/main/java/edu/aku/hassannaqvi/lhwevaluation/models/Form.java
+++ app/src/main/java/edu/aku/hassannaqvi/lhwevaluation/models/Form.java
@@ -939,7 +939,7 @@ public class Form extends BaseObservable implements Observable {
 
     public void setB101(String b101) {
         this.b101 = b101;
-        setB103(b101.equals("2") && this.b102.equals("2") ? this.b103 : "");
+        setB103(b101.equals("1") && this.b102.equals("1") ? "" : this.b103);
         notifyPropertyChanged(BR.b101);
     }
 
@@ -950,7 +950,7 @@ public class Form extends BaseObservable implements Observable {
 
     public void setB102(String b102) {
         this.b102 = b102;
-        setB103(b102.equals("2") && this.b101.equals("2") ? this.b103 : "");
+        setB103(b102.equals("1") && this.b101.equals("1") ? "" : this.b103);
         notifyPropertyChanged(BR.b102);
     }
 
