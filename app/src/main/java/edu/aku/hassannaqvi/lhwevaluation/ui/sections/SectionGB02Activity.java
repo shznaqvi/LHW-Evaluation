2e44ed1c49af49540b6b6aa92ca3a480aa04c99d
Gul Sanober <gulsanobermemon@gmail.com>
2022-09-08 23:19:05 +0500

    g1 and g2 activity added

---
+++ app/src/main/java/edu/aku/hassannaqvi/lhwevaluation/ui/sections/SectionGB02Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.lhwevaluation.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.aku.hassannaqvi.lhwevaluation.R;
+
+public class SectionGB02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_gb02);
+    }
+}
